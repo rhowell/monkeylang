227edea6038df6e9420748e2fc8f57fa9b83528f
Ronnie Howell <ronniemhowell@gmail.com>
2025-08-20 09:12:03 -0400

    feat(ch3.10): Implement function calls

--- object/environment.go
+++ object/environment.go
@@ -2,12 +2,20 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+
+	return env
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
