9b67d37a8960aed297ee53a62d875603646b2040
Ronnie Howell <ronniemhowell@gmail.com>
2025-08-28 21:32:13 -0400

    feat: Add OpAdd to compiler & VM

--- code/code.go
+++ code/code.go
@@ -13,6 +13,7 @@ type (
 
 const (
 	OpConstant Opcode = iota
+	OpAdd
 )
 
 type Definition struct {
@@ -22,6 +23,7 @@ type Definition struct {
 
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
+	OpAdd:      {"OpAdd", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -110,9 +112,11 @@ func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
 	}
 
 	switch operandCount {
+	case 0:
+		return def.Name
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
 	}
 
-	return fmt.Sprintf("ERROR: Unhandled operandCoutn for %s\n", def.Name)
+	return fmt.Sprintf("ERROR: Unhandled operandCount for %s\n", def.Name)
 }
